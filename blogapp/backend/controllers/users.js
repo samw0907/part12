757e659e54b09c1fda804f27bdda562c9932427b
Sam <swilliamson_0907@outlook.com>
2025-01-27 17:06:55 +0200

    part 12 complete

--- blogapp/backend/controllers/users.js
+++ blogapp/backend/controllers/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const usersRouter = express.Router();
 
 usersRouter.get("/", async (request, response) => {
--- blogapp/frontend/vite.config.js
+++ blogapp/frontend/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
-      "/api": {
-        target: "http://localhost:3003",
+      '/api': {
+        target: 'http://backend:3003',
         changeOrigin: true,
       },
     },
   },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./testSetup.js",
+  build: {
+    outDir: 'dist',
   },
-});
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js',
+  }
+})
+
